81535db80baf63f0eac8b9aac745690003250a74
jacksonmalloy <jacksmalloy@gmail.com>
2022-11-12 13:04:03 -0800

    add server compoennt

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,18 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+import { Search } from "../components/Search";
+
+export const revalidate = 3600,
+  dynamic = "force-dynamic";
+
+// This file maps to the index route (/)
+export default async function Page({ searchParams }) {
+  const isSearching = !!searchParams.search;
+
+  console.log({ isSearching });
+
+  return (
+    <section className="p-5">
+      {/* @ts-expect-error Server Component */}
+      <Search />
+    </section>
+  );
 }
---
+++ components/Search.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Search = async () => {
+  return (
+    <div>Search</div>
+  )
+}
